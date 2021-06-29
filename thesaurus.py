93f7fce504b18f37ee647f5db06c976e1c5eb605
Zakharenkov Vladislav <85833732+Craverman@users.noreply.github.com>
2021-06-30 01:15:37 +0300

    Delete thesaurus.py

--- thesaurus.py
+++
@@ -1,9 +0,0 @@
-def thesaurus(*names):
-    name_list = {}
-    for el in names:
-        key = el[0].capitalize()
-        if key not in name_list:
-            name_list[key] = []
-        name_list[key].append(el)
-    return name_list
-print(thesaurus('Mariya', 'Vlad', 'Petr', 'Michael', 'Joseph', 'Victor'))
